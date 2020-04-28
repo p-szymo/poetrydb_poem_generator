744a39772e3dde6317f806823e9e7760022da424
“p-szymo” <joshua.szymanowski@gmail.com>
2020-04-28 18:15:25 -0400

    adding comments, improved word_quantifier

--- functions.py
+++ functions.py
@@ -4,11 +4,16 @@ import string
 import re
 import random
 
+
+## call API and create a list of authors
 def author_grabber(url):
+#     url = "http://poetrydb.org/author"
     authors = requests.get(url)
     authors = [author for author in authors.json()['authors']]
     return authors
 
+
+## call API and create a list of poem titles for each author
 def title_grabber(author):
     base_url = "http://poetrydb.org/author/"
     url_addon = (author + '/title').replace(" ", "%20")
@@ -20,6 +25,9 @@ def title_grabber(author):
         pass
     return titles
 
+
+## call API and create a list of the lines of a poem
+## convert list to a string, joining them with '\n' 
 def poem_grabber(title):
     base_url = 'http://poetrydb.org/title/'
     url_addon = (title + '/lines.json').replace(" ", "%20").replace(')', '').replace(']', '').split('/', 1)[0].split('^', 1)[0]
@@ -34,6 +42,8 @@ def poem_grabber(title):
         pass
     return poem
 
+
+## temporarily change newline and tab characters so they don't disappear during segmentation
 def lines_tabs_creator(po_dict):
     new_line = 'airplane'
     tab = 'automobile'
@@ -47,12 +57,23 @@ def lines_tabs_creator(po_dict):
                 continue
     return po_dict
 
+
+## ask the user to provide a length for the poem (in numerals)
 def word_quantifier():
-    word_count = int(input("What length doth thy sweet nothings require? "))
-    while word_count > 1000:
-        word_count = int(input("Surely thine sweet nothings require less breadth!\nCease skylarking and present me a length of reason! "))
+    while True:
+        try:
+            word_count = int(input("What length doth thy sweet nothings require? "))
+            while word_count > 1000:
+                word_count = int(input("Surely thine sweet nothings require less breadth!\nCease skylarking and present me a length of reason! "))
+        except ValueError:
+            print("Art thou a dullard? Enumerate!")
+            continue
+        else:
+            break
     return word_count
 
+
+## generate poetry from dictionary of words appearing in the PoetryDB database
 def auto_pometizer(po_dict):
     word_count = word_quantifier()
     auto_pome = []
